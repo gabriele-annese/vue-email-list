2bab75145052ecedb39d15d695da872571e913cf
gabriele-annese <gannese06@gmail.com>
2021-11-19 14:41:03 +0100

    prova push email in array

---
+++ main.js
@@ -0,0 +1,33 @@
+// Attraverso l’apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+// Link doc Axios: https://github.com/axios/axios;
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+// Buon lavoro di nuovo e benvenuti nel mondo :api:
+
+
+const app = new Vue({
+    el: "#app",
+    data:{
+        emailArray : [],
+        API: 'https://flynn.boolean.careers/exercises/api/random/mail'
+    },
+    created() {
+        this.genEmail();
+    },
+    methods:{
+        genEmail(){
+        axios.get(this.API)
+        .then(response => {
+        //    console.log(response.data.response)
+           this.emailArray.push(response.data.response)
+           console.log(this.emailArray)
+        })
+        .catch(error => {
+            // handle error
+            console.log(error);
+        })
+        }
+    },
+})
